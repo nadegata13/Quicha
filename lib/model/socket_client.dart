25d9d5465dc46a04ab987e578ad7a9b9b7dd3795
nadegata13 <21cm0113@jec.ac.jp>
2022-12-19 13:37:51 +0900

    通信を実装

--- lib/model/socket_client.dart
+++ lib/model/socket_client.dart
@@ -8,11 +8,17 @@ class SocketClient {
    static SocketClient? _instance;
 
   SocketClient._internal(){
-    socket = IO.io("http://localhost:3000/", <String, dynamic>{
+    //実機テスト用
+    socket = IO.io("http://192.168.3.12:3000/", <String, dynamic>{
       'transports' : ["websocket"],
       'autoConnect' : false,
       'forceNew' : true,
     });
+    // socket = IO.io("http://localhost:3000/", <String, dynamic>{
+    //   'transports' : ["websocket"],
+    //   'autoConnect' : false,
+    //   'forceNew' : true,
+    // });
 
     socket!.connect();
     socket!.onConnect((data) => print(data));
